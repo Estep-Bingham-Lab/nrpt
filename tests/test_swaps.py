96dd9853be5b6d2fd7e33f83eb1f96623b5540ca
Miguel Biron-Lattes <miguelbl9@gmail.com>
2025-05-25 07:19:46 -0700

    all good

--- tests/test_swaps.py
+++ tests/test_swaps.py
@@ -43,15 +43,15 @@ class TestSwaps(unittest.TestCase):
         # check 1 round gives even(odd(initial state))
         rng_key, run_key = random.split(rng_key)
         pt_sampler = initialization.PT(kernel, run_key, n_rounds=1)
-        init_replica_to_chain_idx = pt_sampler.pt_state.replica_to_chain_idx
+        init_chain_to_replica_idx = pt_sampler.pt_state.chain_to_replica_idx
         pt_sampler = sampling.run(pt_sampler)
         pt_state = pt_sampler.pt_state
         self.assertEqual(pt_state.stats.round_idx, 2)
         self.assertEqual(pt_state.stats.scan_idx, 1)
         self.assertTrue(check_maps_inv_temps(pt_state))
         self.assertTrue(jnp.all(
-            pt_state.replica_to_chain_idx == 
-            init_replica_to_chain_idx[
+            pt_state.chain_to_replica_idx == 
+            init_chain_to_replica_idx[
                 pt_sampler.swap_group_actions[1][pt_sampler.swap_group_actions[0]]
             ]
         ))
@@ -59,27 +59,27 @@ class TestSwaps(unittest.TestCase):
         # check `n_replicas` scans reverse the initial permutation
         rng_key, run_key = random.split(rng_key)
         pt_sampler = initialization.PT(kernel, run_key, n_replicas=6, n_rounds=2)
-        init_replica_to_chain_idx = pt_sampler.pt_state.replica_to_chain_idx
+        init_chain_to_replica_idx = pt_sampler.pt_state.chain_to_replica_idx
         pt_sampler = sampling.run(pt_sampler) # 2 rounds == 2+4=6 scans ==> reverse init array
         pt_state = pt_sampler.pt_state
         self.assertEqual(pt_state.stats.round_idx, 3)
         self.assertEqual(pt_state.stats.scan_idx, 1)
         self.assertTrue(check_maps_inv_temps(pt_state))
         self.assertTrue(jnp.all(
-            pt_state.replica_to_chain_idx == jnp.flip(init_replica_to_chain_idx)
+            pt_state.chain_to_replica_idx == jnp.flip(init_chain_to_replica_idx)
         ))
 
         # check 2*n_replicas scans return to the initial permutation
         rng_key, run_key = random.split(rng_key)
         pt_sampler = initialization.PT(kernel, run_key, n_replicas=7, n_rounds=3)
-        init_replica_to_chain_idx = pt_sampler.pt_state.replica_to_chain_idx
+        init_chain_to_replica_idx = pt_sampler.pt_state.chain_to_replica_idx
         pt_sampler = sampling.run(pt_sampler) # 3 rounds == 2+4+8=14 scans ==> return to init array
         pt_state = pt_sampler.pt_state
         self.assertEqual(pt_state.stats.round_idx, 4)
         self.assertEqual(pt_state.stats.scan_idx, 1)
         self.assertTrue(check_maps_inv_temps(pt_state))
         self.assertTrue(jnp.all(
-            pt_state.replica_to_chain_idx == init_replica_to_chain_idx
+            pt_state.chain_to_replica_idx == init_chain_to_replica_idx
         ))
 
         #######################################################################
@@ -87,22 +87,20 @@ class TestSwaps(unittest.TestCase):
         #######################################################################
 
         for is_odd_scan in (0,1):
-            action = pt_sampler.swap_group_actions[is_odd_scan]
-            proposed_replica_to_chain_idx = pt_state.replica_to_chain_idx[action]
             rng_key, run_key = random.split(rng_key)
-            swap_decisions = random.bernoulli(
+            proto_swap_decisions = random.bernoulli(
                 run_key,
-                shape=(len(proposed_replica_to_chain_idx)-1,)
-            )
-            new_replica_to_chain_idx = swaps.resolve_replica_to_chain_idx(
-                pt_state.replica_to_chain_idx,
-                proposed_replica_to_chain_idx,
-                is_odd_scan,
-                swap_decisions
+                shape=(pt_sampler.swap_group_actions.shape[1]-1,)
             )
             new_chain_to_replica_idx = swaps.resolve_chain_to_replica_idx(
-                pt_state.chain_to_replica_idx, 
-                new_replica_to_chain_idx
+                is_odd_scan,
+                proto_swap_decisions,
+                pt_state.chain_to_replica_idx,
+                pt_sampler.swap_group_actions
+            )
+            new_replica_to_chain_idx = swaps.resolve_replica_to_chain_idx(
+                pt_state.replica_to_chain_idx, 
+                new_chain_to_replica_idx
             )
             self.assertTrue(
                 replica_chain_maps_agree(
